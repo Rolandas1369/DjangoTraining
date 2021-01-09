b4b000e2023ed4326da786fa8d4d4ec7eb5ec6a9
Rolandas Butkevicius <rolandaswb@gmail.com>
2021-01-09 12:05:10 +0300

    get urls

--- cvprofile/cv/views.py
+++ cvprofile/cv/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def home(request):
+    cvbankas = requests.get("https://www.cvbankas.lt/?miestas=&padalinys%5B%5D=76&keyw=python")
+    cvonline = requests.get("https://www.cvonline.lt/")
+    cvbankas_status = cvbankas.status_code
+    cvonline_status = cvonline.status_code
+    cont = {'cvbankas_status': cvbankas_status, 'cvonline_status': cvonline_status}
+    return render(request, 'base.html', context=cont)
+
